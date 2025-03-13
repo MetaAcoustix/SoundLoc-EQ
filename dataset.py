ef864cec6db4edb432c070703b01382dd9b80c8b
lincuan <liincuan@icloud.com>
2025-03-13 14:08:26 +0800

    feat: add testing_samples directory for model evaluation
    - Added testing_samples directory with subdirectories:
      - -10DB: Test data with -10 dB noise level.
      - -20DB: Test data with -20 dB noise level.
      - 0DB: Test data with 0 dB noise level.
      - clean: Clean test data without noise.
      - reverb: Test data with reverberation effects.
    - These samples will be used for evaluating model performance across different conditions.
    
    This addition supports comprehensive testing of the unbiased label distribution model.

---
+++ dataset.py
@@ -0,0 +1,6 @@
+"""
+@File: dataset
+@Time: 3/13/25 12:05 PM
+@Author: liincuan
+@Description: 
+"""
---
+++ encoding_decoding.py
@@ -0,0 +1,6 @@
+"""
+@File: encoding_decoding
+@Time: 3/13/25 12:09 PM
+@Author: liincuan
+@Description: 
+"""
